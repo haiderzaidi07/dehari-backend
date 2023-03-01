914c74e08ee5d5e8e23b4215421c5edbc68a24fd
haiderzaidi07 <smhaider072@gmail.com>
2023-03-01 21:47:03 +0500

    made routes and controllers folders

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/database')
+// const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const PORT = 4500
 
 
-require('dotenv').config({path: './config/.env'})
+// require('dotenv').config({path: './config/.env'})
 
-require('./config/passport')(passport)
+// require('./config/passport')(passport)
 
-connectDB()
+// connectDB()
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -17,8 +17,8 @@ app.use(express.json())
 
 
 app.use('/', homeRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/users', userRoutes)
+// app.use('/courses', coursesRoutes)
+// app.use('/users', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
