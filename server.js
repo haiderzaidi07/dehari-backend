c1dd4284336a99573a20eb8c83dae77d5cf41fa8
Haider Zaidi <100717066+haiderzaidi07@users.noreply.github.com>
2023-07-07 18:37:35 +0500

    changed cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://dehari.netlify.app/",
   credentials: true
 }))
 
